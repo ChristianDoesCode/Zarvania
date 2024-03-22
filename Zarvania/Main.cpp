a5199c2805c702cc9033971e2dcac7644a146862
TheChrisCode <christia.phillips@huttoisd.org>
2024-03-21 19:32:57 -0500

    commit 3

--- Zarvania/Main.cpp
+++ Zarvania/Main.cpp
@@ -14,6 +14,7 @@ int main()
 	while(window.isOpen())
 		{
 			sf::Event event;
+
 			while(window.pollEvent(event))
 				{
 					if(event.type == sf::Event::Closed)
@@ -21,6 +22,13 @@ int main()
 						window.close();
 					}
 				}
+
+			//clear the window to black
+			window.clear(sf::Color::Black);
+
+			//Draw stuff here
+
+			window.display();
 		}
 
 	return 0;
